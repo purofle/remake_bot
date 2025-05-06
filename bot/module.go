8edc8d8251823385b99916c994d25e1c8068ea60
purofle <purofle@gmail.com>
2025-05-07 00:04:32 +0800

    feat: add database connection and message counter functionality

--- bot/module.go
+++ bot/module.go
@@ -1,9 +1,15 @@
 package bot
 
-import "go.uber.org/fx"
+import (
+	"github.com/purofle/remake_bot/database"
+	"go.uber.org/fx"
+)
 
 var Module = fx.Options(
+	database.Module,
+
 	fx.Provide(NewRemake),
+	fx.Provide(NewMessageCounter),
 	fx.Provide(NewHandler),
 	fx.Invoke(func(handler *Handler) {
 		handler.RegisterAll()
