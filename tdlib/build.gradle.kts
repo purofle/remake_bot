d212fc564d0fb52862ff93f8e514ba7069c9da79
purofle <purofle@gmail.com>
2026-09-12 00:29:55 +0800

    fix: pin tdlib module to JVM 21 target to fix fatJar in Docker build

--- tdlib/build.gradle.kts
+++ tdlib/build.gradle.kts
@@ -1,6 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     `java-library`
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 group = "com.github.purofle.remakebot"
